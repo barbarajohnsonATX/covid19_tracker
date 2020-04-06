2038cc6f41cc46388da802f5b09057f4055c3ec5
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-04-05 21:09:23 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
-import image from './images/image.png';
+import coronaImage from './images/image.png';
 
 class App extends React.Component {
   state = {
     data: {},
-    country: 'US',
+    country: '',
   }
 
   async componentDidMount() {
-    const data = await fetchData();
+    const fetchedData = await fetchData();
 
-    this.setState({ data });
+    this.setState({ data: fetchedData });
   }
 
   handleCountryChange = async (country) => {
-    const data = await fetchData(country);
+    const fetchedData = await fetchData(country);
+    console.log("handleCountryChange data", fetchedData)
 
-    this.setState({ data, country: country });
+    this.setState({ data: fetchedData, country: country});
   }
 
   render() {
@@ -29,7 +30,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
