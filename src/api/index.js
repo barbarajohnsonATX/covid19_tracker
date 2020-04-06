9b2066d4a32c4177f032cf77c3ce45feb6f4bace
barbarajohnsonATX <johnson.barbara@gmail.com>
2020-04-05 19:26:59 -0500

    added Chart

--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,22 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+         
 
         return { confirmed, recovered, deaths, lastUpdate };
         
     } catch (error) {
+        return error
         
     }
 }
+
+export const fetchDailyData = async () => {
+    try {
+      const { data } = await axios.get(`${url}/daily`);
+      console.log("daily", data)
+      return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+      return error;
+    }
+  };
